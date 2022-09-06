5e33badaa5387360a4cba5d65ba190a0c15b2abe
Vansh3006 <100557683+Vansh3006@users.noreply.github.com>
2022-09-06 23:11:21 +0530

    COMPUTER_GRAPHICS: FixUp: Gave New Header Name to Renamed CG_3.C

--- COMPUTER GRAPHICS/CG_3.C
+++ COMPUTER GRAPHICS/CG_3.C
@@ -1,3 +1,5 @@
+//Experiment 3: Midpoint Circle Generation Algorithm
+
 #include<stdio.h>
 #include<conio.h>
 #include<graphics.h>
@@ -5,46 +7,37 @@
 void main()
 {
   int gd=DETECT,gm,r,x,y,d;
-  printf("Enter radius");
+  printf("Enter radius: ");
   scanf("%d",&r);
-  d=3-2*r;
+  d=1-r;
   x=0;
   y=r;
-  initgraph(&gd,&gm,"c:\\tc\\bgi");
+  initgraph(&gd,&gm,"c:\\turboc3\\bgi");
   while(x<y)
   {
-  delay(10);
-  putpixel(x+100,y+100,4);
-  delay(10);
-  putpixel(y+100,x+100,3);
-  delay(10);
-  putpixel(-x+100,y+100,2);
-  delay(10);
-  putpixel(-y+100,x+100,1);
-  delay(10);
-  putpixel(-x+100,-y+100,4);
-  delay(10);
-  putpixel(-y+100,-x+100,7);
-  delay(10);
-  putpixel(x+100,-y+100,6);
-  delay(10);
-  putpixel(y+100,-x+100,9);
-  delay(10);
-  if(d<0)
-  {
-  d=d+4*x+6;
-  x=x+1;
-  y=y;
-  }
-  else
-  {
-  d=d+4*x-4*y+10;
-  x=x+1;
-  y=y-1;
-  }
+   putpixel(x+100,y+100,4);
+   putpixel(y+100,x+100,3);
+   putpixel(-x+100,y+100,2);
+   putpixel(-y+100,x+100,1);
+   putpixel(-x+100,-y+100,4);
+   putpixel(-y+100,-x+100,7);
+   putpixel(x+100,-y+100,6);
+   putpixel(y+100,-x+100,9);
+   if(d<0)
+   {
+    d=d+2*x+3;
+    x=x+1;
+    y=y;
+   }
+   else
+   {
+    d=d+2*x-2*y+5;
+    x=x+1;
+    y=y-1;
+   }
   }
   getch();
-  }
+}
 
 
 
