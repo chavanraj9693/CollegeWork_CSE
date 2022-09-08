01566718c4f5c080c32c02369db114a18a8e19c9
Vansh3006 <100557683+Vansh3006@users.noreply.github.com>
2022-09-08 20:24:28 +0530

    COMPUTER_GRAPHICS: Rename Files CG_3.C ---> CG_1.C

--- COMPUTER GRAPHICS/CG_3.C
+++ COMPUTER GRAPHICS/CG_3.C
@@ -1,33 +1,62 @@
-//Experiment 1: DDA Line Drawing Algorithm
-
 #include<stdio.h>
 #include<conio.h>
 #include<graphics.h>
 #include<math.h>
 void main()
 {
-  int gd=DETECT,gm,i;
-  float x1,x2,y1,y2,dx,dy,length,x,y;
-  printf("\n Enter value of x1,y1,x2,y2");
-  scanf("%f%f%f%f",&x1,&y1,&x2,&y2);
-  dx=abs(x2-x1);
-  dy=abs(y2-y1);
-  if(dx>dy)
-  length=dx;
-  else
-  length=dy;
-  dx=(x2-x1)/length;
-  dy=(y2-y1)/length;
-  i=0;
-  x=x1;
-  y=y1;
+  int gd=DETECT,gm,r,x,y,d;
+  printf("Enter radius");
+  scanf("%d",&r);
+  d=3-2*r;
+  x=0;
+  y=r;
   initgraph(&gd,&gm,"c:\\tc\\bgi");
-  while(i<=length)
+  while(x<y)
   {
-  putpixel(x,y,4);
-  x=x+dx;
-  y=y+dy;
-  i++;
+  delay(10);
+  putpixel(x+100,y+100,4);
+  delay(10);
+  putpixel(y+100,x+100,3);
+  delay(10);
+  putpixel(-x+100,y+100,2);
+  delay(10);
+  putpixel(-y+100,x+100,1);
+  delay(10);
+  putpixel(-x+100,-y+100,4);
+  delay(10);
+  putpixel(-y+100,-x+100,7);
+  delay(10);
+  putpixel(x+100,-y+100,6);
+  delay(10);
+  putpixel(y+100,-x+100,9);
+  delay(10);
+  if(d<0)
+  {
+  d=d+4*x+6;
+  x=x+1;
+  y=y;
+  }
+  else
+  {
+  d=d+4*x-4*y+10;
+  x=x+1;
+  y=y-1;
+  }
   }
   getch();
   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
